14f40f5fb02419913875912f609fd68b324f1fc8
YeIIowMinion <139129274+YeIIowMinion@users.noreply.github.com>
2024-04-14 14:27:19 +0300

    sadj

--- header.js
+++ header.js
@@ -18,3 +18,38 @@ faBars.addEventListener("click", function(){
         faX.classList.remove("faX")
     }
 })
+
+let cucumbers = document.querySelector(".Cucumbers")
+let tomatoes = document.querySelector(".Tomatoes")
+let carrots = document.querySelector(".Carrots")
+let beets = document.querySelector(".Beets")
+
+let cucumberImg = document.querySelector(".cucumberImg")
+let tomatoeImg = document.querySelector(".tomatoeImg")
+let carrotsImg = document.querySelector(".carrotsImg")
+let beetImg = document.querySelector(".beetImg")
+
+cucumbers.addEventListener("mouseenter", function(){
+    cucumberImg.classList.add("img")
+    beetImg.classList.remove("img")
+    tomatoeImg.classList.remove("img")
+    carrotsImg.classList.remove("img")
+})
+tomatoes.addEventListener("mouseenter", function(){
+    tomatoeImg.classList.add("img")
+    cucumberImg.classList.remove("img")
+    carrotsImg.classList.remove("img")
+    beetImg.classList.remove("img")
+})
+carrots.addEventListener("mouseenter", function(){
+    carrotsImg.classList.add("img")
+    tomatoeImg.classList.remove("img")
+    cucumberImg.classList.remove("img")
+    beetImg.classList.remove("img")
+})
+beets.addEventListener("mouseenter", function(){
+    beetImg.classList.add("img")
+    carrotsImg.classList.remove("img")
+    cucumberImg.classList.remove("img")
+    tomatoeImg.classList.remove("img")
+})
